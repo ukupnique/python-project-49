d9bf1b936bda1f3f0b8642f01f472ded22bac37c
Eugen <ukupnique@gmail.com>
2024-10-26 20:13:43 +0300

    step 9

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -4,23 +4,21 @@ from brain_games.engine import run_game
 RULES = 'What number is missing in the progression?'
 
 
+def generate_question():
+    row = gen_row()
+    pos = random.randint(0, 4)
+    correct_answer = row[pos]
+    row[pos] = '..'
+    question = ' '.join(map(str, row))
+    return question, correct_answer
+
+
 def gen_row():
     num_start = random.randint(1, 100)
     num = random.randint(1, 5)
     nums = []
     for i in range(10):
         num_start += num
-        nums.append(num_start)
-    return nums
-
-
-def gen_row():
-    num_start = random.randint(1, 100)
-    num2 = random.randint(1, 5)
-    mul = random.randint(1, 5)
-    nums = []
-    for i in range(10):
-        num_start += num2 * (mul + i)
         nums.append(num_start)
     return nums
 
